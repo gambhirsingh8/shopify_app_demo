89bef0b25da7def4ea1f5eb1a9dac7aac436661b
Gambhir Singh <gambhirsingh8@gmail.com>
2016-05-16 18:44:41 +0530

    file upload

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,10 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-       $asset = $shopify('PUT /admin/themes/assets.json', array(), array
-    (
-        'asset' => array
-        (
-            "key" => "snippets/file.liquid",
-            "src" => "file.liquid"
-        )
-    ));
+   
+	
+	ShopifyAPI::Asset.create(key: 'templates/collection.alternate.liquid', src: 'collection.alternate.liquid')
+	ShopifyAPI::Asset.create(key: 'snippets/snippet-select.liquid', src: 'snippet-select.liquid')
+	
+	
   end
 end
