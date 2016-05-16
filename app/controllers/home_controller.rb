a9ff0adc0ea88a3d3ef333e7acae020d9e83b082
Gambhir Singh <gambhirsingh8@gmail.com>
2016-05-16 18:17:47 +0530

    shopify file upload

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
    
 	
-	ShopifyAPI::Asset.create(key: 'templates/collection.alternate.liquid', src: 'myapp.liquid')
+	ShopifyAPI::Asset.create(key: 'templates/collection.alternate.liquid', src: 'collection.alternate.liquid')
 	
 	
   end
