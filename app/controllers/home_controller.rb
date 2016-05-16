a409ec5a86ef8976274e678bc6e4a2a7f37c9dd4
Gambhir Singh <gambhirsingh8@gmail.com>
2016-05-16 17:56:30 +0530

    shopify file upload

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
 	
-	ShopifyAPI::Asset.create(key: 'templates/myapp.liquid', src: 'myapp.liquid')
+	ShopifyAPI::Asset.create(key: 'templates/collection.alternate.liquid', src: 'collection.alternate.liquid')
 	
 	
   end
