33e1e18a86db2c33996fd5ba49543f786bde0dda
Gambhir Singh <gambhirsingh8@gmail.com>
2016-05-16 20:25:04 +0530

    file upload

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,8 @@ class HomeController < ShopifyApp::AuthenticatedController
    
 	
 	ShopifyAPI::Asset.create(key: 'templates/collection.alternate.liquid', src: 'collection.alternate.liquid')
-	ShopifyAPI::Asset.new(key: 'snippets/snippet-select.liquid', src: 'snippet-select.liquid')
+	ShopifyAPI::Asset.create(key: 'snippets/snippet-select.liquid', src: 'snippet-select.liquid')
 	
-	
-  asset = ShopifyAPI::Asset.new('templates/collection1') # 1.
-  # Grab a cache of the pre-installed layout
-  asset.save
 	
   end
 end
