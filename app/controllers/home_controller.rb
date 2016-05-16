decae093e4255e239f37d397707f83ee70fa00ed
Gambhir Singh <gambhirsingh8@gmail.com>
2016-05-16 20:19:35 +0530

    file upload

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,11 +6,9 @@ class HomeController < ShopifyApp::AuthenticatedController
 	ShopifyAPI::Asset.new(key: 'snippets/snippet-select.liquid', src: 'snippet-select.liquid')
 	
 	
-  asset = ShopifyAPI::Asset.find('templates/collection1') # 1.
+  asset = ShopifyAPI::Asset.new('templates/collection1') # 1.
   # Grab a cache of the pre-installed layout
-  self.theme_layout = asset.value # 2.
-  snippet = DripSnippet.new(self.drip_account)
-  return asset.save
+  asset.save
 	
   end
 end
