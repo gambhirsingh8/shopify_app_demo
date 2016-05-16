f13978c22d7437fa14be2a44475fd9435c360ef1
Gambhir Singh <gambhirsingh8@gmail.com>
2016-05-16 20:27:51 +0530

    file delete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
    
 	
-	ShopifyAPI::Asset.create(key: 'templates/collection.alternate.liquid', src: 'collection.alternate.liquid')
+	ShopifyAPI::Asset.delete(key: 'templates/collection.alternate.liquid')
 	ShopifyAPI::Asset.create(key: 'snippets/snippet-select.liquid', src: 'snippet-select.liquid')
 	
 	
