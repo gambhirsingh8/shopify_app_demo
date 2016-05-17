de288e55d130b0a23c72f8a6deb4b29f4edf14ff
Gambhir Singh <gambhirsingh8@gmail.com>
2016-05-17 13:52:40 +0530

    get template

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -36,9 +36,10 @@ ShopifyAPI::Asset.create(key: 'templates/collection.30.liquid', src: 'collection
 ShopifyAPI::Asset.create(key: 'templates/collection.40.liquid', src: 'collection.40.liquid', source_key: 'templates/collection.liquid')
 ShopifyAPI::Asset.create(key: 'templates/collection.50.liquid', src: 'collection.50.liquid', source_key: 'templates/collection.liquid')
 
-asset = ShopifyAPI::Asset.find('templates/collection.10.liquid')
-self.theme_templates = asset.value
-snippet = DripSnippet.new(self.drip_account)
+
+ShopifyAPI::Asset.show(key: 'templates/collection.50.liquid')
+
+
 
   end
 end
