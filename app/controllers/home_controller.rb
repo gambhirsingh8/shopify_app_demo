4a2b0c1f6f096eb2f286eabe4ada93d9d5426093
Gambhir Singh <gambhirsingh8@gmail.com>
2016-05-17 10:49:45 +0530

    code for content update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,15 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-   
-	
-	ShopifyAPI::Asset.create(key: 'templates/collection.alternate.liquid')
-	ShopifyAPI::Asset.create(key: 'snippets/snippet-select.liquid', src: 'snippet-select.liquid')
-	
-	
+ 
+  asset = ShopifyAPI::Asset.find('snippets/snippet-select.liquid') 
+ 
+  self.theme_snippets = asset.value 
+  snippet = DripSnippet.new(self.drip_account)
+  if asset.value.match(/#{snippet.start_wrapper}/i)
+    asset.value.sub!(/#{snippet.start_wrapper}.*#{snippet.end_wrapper}/im, snippet.snippet)
+  else
+    asset.value.sub!(/<\/body>/, "#{snippet.snippet}</body>") 
+  end
+  return asset.save	
   end
 end
