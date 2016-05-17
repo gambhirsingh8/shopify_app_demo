19b8146835cc18c0b266b386b44be9bec415dda9
Gambhir Singh <gambhirsingh8@gmail.com>
2016-05-17 11:12:08 +0530

    code for content update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ShopifyApp::AuthenticatedController
   if asset.value.match(/#{snippet.start_wrapper}/i)
     asset.value.sub!(/#{snippet.start_wrapper}.*#{snippet.end_wrapper}/im, snippet.snippet)
   else
-    asset.value.sub!(/<\/body>/, "#{snippet.snippet}</body>") 
+    asset.value.sub!("#{snippet.snippet}") 
   end
   return asset.save	
   end
