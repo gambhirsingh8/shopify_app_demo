d071dea7e4906017cc748e567cc83360823d13c1
Gambhir Singh <gambhirsingh8@gmail.com>
2016-05-17 13:03:07 +0530

    file upload

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -36,6 +36,8 @@ ShopifyAPI::Asset.create(key: 'templates/collection.30.liquid', src: 'collection
 ShopifyAPI::Asset.create(key: 'templates/collection.40.liquid', src: 'collection.40.liquid', source_key: 'templates/collection.liquid')
 ShopifyAPI::Asset.create(key: 'templates/collection.50.liquid', src: 'collection.50.liquid', source_key: 'templates/collection.liquid')
 
+ShopifyAPI::Asset.get{key: 'templates/collection.10.liquid'}
+
 	
   end
 end
